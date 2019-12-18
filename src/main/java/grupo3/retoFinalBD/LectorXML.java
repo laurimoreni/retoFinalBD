bae2a30e034a9ab93a1721c58698d4df64ef9703
xpCRYSISqx <mikel.gozaloqu@elorrieta-errekamari.com>
2019-12-18 09:55:22 +0100

    Lector del XML de Alojamientos terminado (creo)

--- src/main/java/grupo3/retoFinalBD/LectorXML.java
+++ src/main/java/grupo3/retoFinalBD/LectorXML.java
@@ -16,6 +16,8 @@ public class LectorXML {
 	public ArrayList<Alojamiento> CargarAlojamientos(String path, ArrayList<Alojamiento> alojamientos){
 		File archivo = new File(path);
 		Formatos formato = new Formatos();
+		ArrayList<Provincia> provincias = new ArrayList<Provincia>();
+		int id = 0;
 		try {
 			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder builder = factory.newDocumentBuilder();
@@ -38,11 +40,43 @@ public class LectorXML {
 					alojamiento.setTurismemail(elemento.getElementsByTagName("turismemail").item(0).getTextContent());
 					alojamiento.setWeb(elemento.getElementsByTagName("web").item(0).getTextContent());
 					alojamiento.setMarks(elemento.getElementsByTagName("marks").item(0).getTextContent());
+					alojamiento.setMunicipality(elemento.getElementsByTagName("municipalitiy").item(0).getTextContent());
+					if(i == 0) {
+						provincias = CargarProvincias(elemento.getElementsByTagName("territory").item(0).getTextContent(), provincias, id);
+						id++;
+					} else {
+						for(Provincia p: provincias) {
+							if(p.getNombre().equals(elemento.getElementsByTagName("territory").item(0).getTextContent())) {
+								alojamiento.setProvincia(p);
+							} else {
+								provincias = CargarProvincias(elemento.getElementsByTagName("territory").item(0).getTextContent(), provincias, id);
+								id++;
+							}
+						}
+					}
+					alojamiento.setLatwgs84(Float.parseFloat(elemento.getElementsByTagName("latwgs84").item(0).getTextContent()));
+					alojamiento.setLonwgs84(Float.parseFloat(elemento.getElementsByTagName("lonwgs84").item(0).getTextContent()));
+					alojamiento.setPostalcode(Integer.parseInt(elemento.getElementsByTagName("postalcode").item(0).getTextContent()));
+					alojamiento.setCapacity(Integer.parseInt(elemento.getElementsByTagName("capacity").item(0).getTextContent()));
+					alojamiento.setRestaurant(Integer.parseInt(elemento.getElementsByTagName("restaurant").item(0).getTextContent()));
+					alojamiento.setStore(Integer.parseInt(elemento.getElementsByTagName("store").item(0).getTextContent()));
+					alojamiento.setAutocaravana(Integer.parseInt(elemento.getElementsByTagName("autocaravana").item(0).getTextContent()));
+					
+					alojamientos.add(alojamiento);
 				}
 			}
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 		return alojamientos;
+	}
+	
+	public ArrayList<Provincia> CargarProvincias(String nombre, ArrayList<Provincia> provincias, int id){
+		Provincia provincia = new Provincia();
+		
+		provincia.setNombre(nombre);
+		provincia.setId(id);
+		provincias.add(provincia);
+		return provincias;
 	}
 }
