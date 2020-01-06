ea9497ee91aae2cbc9b625f69ca87190e8d23b18
José Manuel Muñoz Barragán <josemanuel.munozba@elorrieta-errekamari.com>
2020-01-06 09:48:20 +0100

    Mensaje de error de conexión a BD

--- src/main/java/grupo3/retoFinalBD/Principal.java
+++ src/main/java/grupo3/retoFinalBD/Principal.java
@@ -18,47 +18,49 @@ public class Principal {
 	public void procesoPpal() {
 		// iniciar sesion hibernate
 		vista.textArea.append("Conectando a la base de datos...\n");
-		Session session = HibernateUtil.getSessionFactory().openSession();
-		session.beginTransaction();
-		vista.textArea.append("Conectado!\n");
-		
-		CargarProvincias cargarProvincias = new CargarProvincias();
-		ArrayList<Provincia> provincias = new ArrayList<Provincia>();
-		provincias = cargarProvincias.cargarProvincias(provincias);
-		
-		// leer ficheros XML
-		LeerFicheros leer = new LeerFicheros(vista);
-		ArrayList<Alojamiento> alojamientos = new ArrayList<Alojamiento>();
-		alojamientos = leer.leerFicheroXML(alojamientos, provincias);
-		
-		// guardar datos en BD
-		//Comprobar si ya están las provincias en la BD
-		List<Provincia> provinciasBD = session.createQuery("from Provincia").list();
-		if (provinciasBD.size() == 0) {
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			session.beginTransaction();
+			vista.textArea.append("Conectado!\n");
 			
-			for(Provincia provincia: provincias) {
-				session.save(provincia);
+			CargarProvincias cargarProvincias = new CargarProvincias();
+			ArrayList<Provincia> provincias = new ArrayList<Provincia>();
+			provincias = cargarProvincias.cargarProvincias(provincias);
+			
+			// leer ficheros XML
+			LeerFicheros leer = new LeerFicheros(vista);
+			ArrayList<Alojamiento> alojamientos = new ArrayList<Alojamiento>();
+			alojamientos = leer.leerFicheroXML(alojamientos, provincias);
+			
+			// guardar datos en BD
+			//Comprobar si ya están las provincias en la BD
+			List<Provincia> provinciasBD = session.createQuery("from Provincia").list();
+			if (provinciasBD.size() == 0) {
+				
+				for(Provincia provincia: provincias) {
+					session.save(provincia);
+				}
 			}
+			
+			for (Alojamiento alojamiento : alojamientos) {
+				session.save(alojamiento);
+			}
+			session.getTransaction().commit();
+			
+			// leer datos de la BD
+			// LecturaBD lectur = new LecturaBD();
+			
+			// escribir los datos en archivo JSON
+			vista.textArea.append("Exportando JSON...\n");
+			LectorJSON json = new LectorJSON();
+			json.convertirAJson(alojamientos);
+			vista.textArea.append("JSON exportado.\n");
+			// cerrar sesion hibernate
+			HibernateUtil.shutdown();
+		} catch (ExceptionInInitializerError ex) {
+			vista.textArea.append("Error en la conexión a la Base de Datos\n");
+		} finally {
+			vista.btnOk.setEnabled(true);
 		}
-		
-		for (Alojamiento alojamiento : alojamientos) {
-			session.save(alojamiento);
-		}
-		session.getTransaction().commit();
-		
-		// leer datos de la BD
-		// LecturaBD lectur = new LecturaBD();
-		
-		// escribir los datos en archivo JSON
-		vista.textArea.append("Exportando JSON...\n");
-		LectorJSON json = new LectorJSON();
-		json.convertirAJson(alojamientos);
-		vista.textArea.append("JSON exportado.\n");
-	
-		// cerrar sesion hibernate
-		HibernateUtil.shutdown();
-		
-		vista.btnOk.setEnabled(true);
 	}
 			
 }
